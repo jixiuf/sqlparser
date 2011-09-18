7779b41703ef4842deb5ab8ac107b11b9bffbf99
jixiuf <jixiuf@gmail.com>
2011-09-19 01:55:07 +0800

    unfinished 2

--- sqlserver-query.el
+++ sqlserver-query.el
@@ -56,27 +56,16 @@
 ;;
 ;; Below are complete command list:
 ;;
-;;  `sqlserver-query-init-interactive'
-;;    set server dbname username password interactive
-;;  `sqlserver-query-rebuild-connection'
-;;    rebuild connection.
+;;  `sqlserver-query-create-connection'
+;;    open connection with sqlcmd.exe or osql.exe.
 ;;
 ;;; Customizable Options:
 ;;
 ;; Below are customizable option list:
 ;;
-;;  `sqlserver-username'
-;;    sqlserver user name.
-;;    default = "sa"
-;;  `sqlserver-password'
-;;    sqlserver user password.
-;;    default = "sa"
-;;  `sqlserver-server-instance'
-;;    Default server or host.
-;;    default = "localhost\\SQLEXPRESS"
-;;  `sqlserver-dbname'
-;;    database name .
-;;    default = "master"
+;;  `sqlserver-connection-info'
+;;    sqlserver connection info .
+;;    default = (quote ((username . "sa") (password . "sa") (server-instance . "localhost\\SQLEXPRESS") (dbname . "master")))
 ;;  `sqlserver-cmd'
 ;;    sqlserver-cmd  now  support sqlcmd.exe and osql.exe
 ;;    default = (quote sqlcmd)
@@ -88,27 +77,37 @@
 (defgroup sqlserver-query nil
   "SQL SERVER QUERY"
   :group 'SQL)
-(defcustom sqlserver-username "sa"
-  "sqlserver user name."
+(defcustom sqlserver-connection-info
+  '((username . "sa")
+    (password . "sa")
+    (server-instance . "localhost\\SQLEXPRESS")
+    (dbname . "master"))
+  "sqlserver connection info ."
   :group 'sqlserver-query
-  :type 'string)
+  :type 'alist)
+(make-variable-buffer-local 'sqlserver-connection-info)
 
-(defcustom sqlserver-password "sa"
-  "sqlserver user password."
-  :group 'sqlserver-query
-  :type 'string)
+;; (defcustom sqlserver-username "sa"
+;;   "sqlserver user name."
+;;   :group 'sqlserver-query
+;;   :type 'string)
 
-(defcustom sqlserver-server-instance "localhost\\SQLEXPRESS"
-  "Default server or host."
-  :type 'string
-  :group 'sqlserver-query
-  :safe 'stringp)
+;; (defcustom sqlserver-password "sa"
+;;   "sqlserver user password."
+;;   :group 'sqlserver-query
+;;   :type 'string)
 
-(defcustom sqlserver-dbname "master"
-  "database name ."
-  :type 'string
-  :group 'sqlserver-query
-  :safe 'stringp)
+;; (defcustom sqlserver-server-instance "localhost\\SQLEXPRESS"
+;;   "Default server or host."
+;;   :type 'string
+;;   :group 'sqlserver-query
+;;   :safe 'stringp)
+
+;; (defcustom sqlserver-dbname "master"
+;;   "database name ."
+;;   :type 'string
+;;   :group 'sqlserver-query
+;;   :safe 'stringp)
 
 (defcustom sqlserver-cmd 'sqlcmd
   "sqlserver-cmd  now  support sqlcmd.exe and osql.exe
@@ -165,76 +164,135 @@ sqlserver 2005 add new cmd sqlcmd.exe. and osql.exe is not recommended."
       (sqlserver-parse-result-as-list-4-sqlcmd raw-result)
     (sqlserver-parse-result-as-list-4-osql raw-result)))
 
-(defun sqlserver-conn-str()
+;; (sqlserver-format-connect-string sqlserver-connection-info)
+(defun sqlserver-format-connect-string(connection-info)
   "default:sqlcmd -S localhost\\SQLEXPRESS -U sa -P sa -d master -h-1 -w 65535   -s \"\^E\" -W"
   (if (equal sqlserver-cmd 'sqlcmd)
       (format "%s -S %s -U %s -P %s -d %s -h-1  -w 65535  -s \"\^E\" -W"
-              (symbol-name sqlserver-cmd) sqlserver-server-instance
-	      sqlserver-username sqlserver-password sqlserver-dbname)
+              (symbol-name sqlserver-cmd)
+              (cdr (assoc 'server-instance connection-info))
+              (cdr (assoc 'username connection-info))
+              (cdr (assoc 'password connection-info))
+              (cdr (assoc 'dbname connection-info)))
     (format "%s -S %s -U %s -P %s -d %s -h-1  -n -w 65535 -s \"\" "
-	    ;;            "%s -S %s -U %s -P %s -d %s -h-1  -n -w 65535  -s \"\^E\""
-            (symbol-name sqlserver-cmd) sqlserver-server-instance
-	    sqlserver-username sqlserver-password sqlserver-dbname)))
+            ;;            "%s -S %s -U %s -P %s -d %s -h-1  -n -w 65535  -s \"\^E\""
+            (symbol-name sqlserver-cmd)
+            (cdr (assoc 'server-instance connection-info))
+            (cdr (assoc 'username connection-info))
+            (cdr (assoc 'password connection-info))
+            (cdr (assoc 'dbname connection-info)))))
 
-;;;###autoload
-(defun sqlserver-query-init-interactive()
+
+(defun sqlserver-query-read-connect-string()
   "set server dbname username password interactive"
-  (interactive)
-  (setq sqlserver-server-instance
-        (read-string (format  "sqlserver-server-instance(default:%s):"
-			      sqlserver-server-instance)
-                     "" nil sqlserver-server-instance))
-  (setq sqlserver-dbname
-        (read-string (format  "sqlserver-db(default:%s):" sqlserver-dbname)
-                     "" nil sqlserver-dbname))
-  (setq sqlserver-username
-        (read-string (format  "sqlserver-username(default:%s):" sqlserver-username)
-                     "" nil sqlserver-username))
-  (setq sqlserver-password (read-passwd "sqlserver-password:"  nil sql-password))
-  (sqlserver-query-init))
+  (let ((connection-info (copy-alist sqlserver-connection-info)))
+    (setcdr  (assoc 'username connection-info)
+             (read-string (format  "username(default:%s):"  (cdr (assoc 'username connection-info)))
+                          "" nil   (cdr (assoc 'username connection-info))))
+    (setcdr  (assoc 'password connection-info)
+             (read-passwd "password:"  nil ))
+    (setcdr  (assoc 'username connection-info)
+             (read-string (format  "server-instance(default:%s):"  (cdr (assoc 'server-instance connection-info)))
+                          "" nil (cdr (assoc 'server-instance connection-info))))
+    (setcdr  (assoc 'dbname connection-info)
+             (read-string (format  "dbname(default:%s):"  (cdr (assoc 'dbname connection-info)))
+                          "" nil (cdr (assoc 'dbname connection-info))))
+    connection-info))
 
-(defun sqlserver-query-init()
+(defun sqlserver-query-create-connection(connection-info)
   "open connection with sqlcmd.exe or osql.exe."
-  (setq sqlserver-query-process
-        (start-process-shell-command (symbol-name sqlserver-cmd)
-				     sqlserver-query-buffer (sqlserver-conn-str)))
-  (set-process-query-on-exit-flag sqlserver-query-process nil)
-  (set-process-filter sqlserver-query-process 'sqlserver-filter-fun))
+  (interactive (list (sqlserver-query-read-connect-string)))
+  (let ((process (start-process-shell-command
+                  (symbol-name sqlserver-cmd)
+                  (concat " *sqlserver-query-" (number-to-string (random)) "*")
+                  (concat  (sqlserver-format-connect-string connection-info)))))
+
+    (set-process-query-on-exit-flag process nil)
+    (set-process-sentinel
+     process
+     (lambda (proc change)
+       (when (string-match "\\(finished\\|exited\\|exited abnormally with code\\)" change)
+         (message  (concat  (process-name proc) " exited"))
+         (kill-buffer  (process-buffer proc)))))
+    (list process
+          (process-buffer process)
+          connection-info)
+    ;; (set-process-filter process 'sqlserver-filter-fun)
+    ))
+;; (sqlserver-query-create-connection sqlserver-connection-info)
 
 ;;;###autoload
-(defun sqlserver-query-rebuild-connection()
-  "rebuild connection."
-  (interactive)
-  (if (and   sqlserver-query-process
-             (equal (process-status sqlserver-query-process ) 'run))
-      (progn
-        (process-send-string sqlserver-query-process  "go\n")
-        (process-send-string sqlserver-query-process  "exit\n")
-        (set-process-sentinel sqlserver-query-process
-                              (lambda (proc change)
-                                (when (string-match "\\(finished\\|exited\\)" change)
-                                  (sqlserver-query-init)))))
-    (sqlserver-query-init)))
+(defun sqlserver-query-close-connection(connection)
+  "close connection.kill sqlplus process and buffer ."
+  (kill-process (nth 0 connection))
+  (kill-buffer (nth 1 connection))
+  (setq connection nil))
+
+(defvar sqlserver-query-default-connection nil)
+
+;;(sqlserver-query "select empno from emp")
+;; (sqlserver-query "select empno from emp" (sqlserver-query-create-connection "scott/tiger"))
+;;;###autoload
+(defun sqlserver-query (sql &optional sqlserver-query-connection)
+  "execute sql using `sqlcmd' or `osql' ,and return the result of it."
+  (let( (connection sqlserver-query-connection) process)
+    (unless connection
+      (unless sqlserver-query-default-connection
+        (setq sqlserver-query-default-connection (call-interactively 'sqlserver-query-create-connection)))
+      (setq connection sqlserver-query-default-connection)
+      )
+    (setq process (car connection))
+    (when (string-match "\\(.*\\);[ \t]*" sql)
+      (setq sql (match-string 1 sql)))
+    (with-current-buffer (process-buffer process)
+      (delete-region (point-min) (point-max))
+      (let ((start (point-min)) end)
+        (goto-char (point-max))
+        (process-send-string process (format "%s ;\n" sql))
+        (process-send-string process  "go\n")
+        (goto-char (point-min))
+        (while (not (re-search-forward "(.*\\(行受影响\\|rows affected\\))" nil t 1))
+          (when (accept-process-output process  sqlserver-timeout-wait-for-result 0 nil)
+            (goto-char (point-min))))
+        (setq end (1- (match-beginning 0)))
+        (when (re-search-backward "\\bSQL> " nil t 1) (setq start (match-end 0))) ;FIXME
+        (sqlserver-parse-result-as-list  (buffer-substring-no-properties start end))
+        ))))
+
+;; ;;;###autoload
+;; (defun sqlserver-query-rebuild-connection()
+;;   "rebuild connection."
+;;   (interactive)
+;;   (if (and   sqlserver-query-process
+;;              (equal (process-status sqlserver-query-process ) 'run))
+;;       (progn
+;;         (process-send-string sqlserver-query-process  "go\n")
+;;         (process-send-string sqlserver-query-process  "exit\n")
+;;         (set-process-sentinel sqlserver-query-process
+;;                               (lambda (proc change)
+;;                                 (when (string-match "\\(finished\\|exited\\)" change)
+;;                                   (sqlserver-query-create-connection)))))
+;;     (sqlserver-query-create-connection)))
 
 
 
-(defun sqlserver-query (sql)
-  "geta result from the function `sqlserver-query-result-function'
-after you call `sqlserver-query'"
-  (unless (and (buffer-live-p (get-buffer  sqlserver-query-buffer))
-           sqlserver-query-process
-           (equal (process-status sqlserver-query-process ) 'run))
-      (sqlserver-query-rebuild-connection))
-  (when (string-match "\\(.*\\);[ \t]*" sql)
-    (setq sql (match-string 1 sql)))
-  (process-send-string sqlserver-query-process  (format "%s ;\n" sql))
-  (process-send-string sqlserver-query-process  "go\n")
-  (if (accept-process-output sqlserver-query-process  sqlserver-timeout-wait-for-result 0 nil)
-      sqlserver-query-result
-    nil))
+;; (defun sqlserver-query (sql)
+;;   "geta result from the function `sqlserver-query-result-function'
+;; after you call `sqlserver-query'"
+;;   (unless (and (buffer-live-p (get-buffer  sqlserver-query-buffer))
+;;            sqlserver-query-process
+;;            (equal (process-status sqlserver-query-process ) 'run))
+;;       (sqlserver-query-rebuild-connection))
+;;   (when (string-match "\\(.*\\);[ \t]*" sql)
+;;     (setq sql (match-string 1 sql)))
+;;   (process-send-string sqlserver-query-process  (format "%s ;\n" sql))
+;;   (process-send-string sqlserver-query-process  "go\n")
+;;   (if (accept-process-output sqlserver-query-process  sqlserver-timeout-wait-for-result 0 nil)
+;;       sqlserver-query-result
+;;     nil))
 
-(defun sqlserver-filter-fun (process output)
-  (setq  sqlserver-query-result  ( sqlserver-parse-result-as-list  output)))
+;; (defun sqlserver-filter-fun (process output)
+;;   (setq  sqlserver-query-result  ( sqlserver-parse-result-as-list  output)))
 
 (provide 'sqlserver-query)
 ;;; sqlserver-query.el ends here
