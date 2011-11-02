ccfaacc06627f36a3f33bafa5f53ed7e7099b111
jixiuf <jixiuf@gmail.com>
2011-11-02 13:03:40 +0800

    (require 'thingatpt)

--- sqlparser-sqlserver-complete.el
+++ sqlparser-sqlserver-complete.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2011 Joseph
 
 ;; Created: 2011年08月19日 星期五 00时38分17秒
-;; Last Updated: Joseph 2011-11-02 11:06:30 星期三
+;; Last Updated: Joseph 2011-11-02 13:03:37 星期三
 ;; Version: 0.1.2
 ;; Author: Joseph  jixiuf@gmail.com
 ;; Keywords: sql complete sqlserver
@@ -102,6 +102,7 @@
 
 
 (require 'sqlserver-query)
+(require 'thingatpt)
 (require 'anything nil t)
 
 (defvar sqlparser-sqlserver-connection nil)
@@ -591,20 +592,19 @@ it will return 'table' ,or 'column' ,or nil.
 
 (defun sqlparser-get-prefix-4-sqlserver()
   "for example `tablename.col' `table.' `str'"
-  (let ((init-pos (point)) prefix)
-    (when (search-backward-regexp "[ \t\n]+\\|[,\\();=\\+-\\*/%]" (point-min) t)
-      (setq prefix (buffer-substring (match-end 0) init-pos)))
-    (goto-char init-pos)
-    (or prefix "")
-    ))
+  (with-syntax-table (copy-syntax-table (syntax-table))
+    (modify-syntax-entry ?.  "w");treat . as part of word
+    (modify-syntax-entry ?[  "w");treat [ as part of word
+    (modify-syntax-entry ?]  "w");treat ] as part of word
+    (or (thing-at-point 'word) "")))
 
 (defun sqlparser-word-before-point-4-sqlserver()
   "get word before current point or empty string."
-  (save-excursion
-    (let ((current-pos (point)))
-      (if (search-backward-regexp "\s-\\|[ \t\n\r]+\\|[\\.,\\()-=%/]" (point-at-bol) t)
-          (buffer-substring-no-properties (match-end 0) current-pos)
-        ""))))
+  (with-syntax-table (copy-syntax-table (syntax-table))
+    (modify-syntax-entry ?.  ".");treat . as punctuation character
+    (modify-syntax-entry ?[  ".");treat [ as punctuation character
+    (modify-syntax-entry ?]  ".");treat ] as punctuation character
+    (or (thing-at-point 'word) "")))
 
 (provide 'sqlparser-sqlserver-complete)
 ;;; sqlparser-sqlserver-complete.el ends here
